ba4e2bf3e398111375b7edd912e185191e8e45ad
Tadesse Adhanom <103166432+Tadhanom95@users.noreply.github.com>
2022-06-01 01:40:03 +0000

    Adds form handler

--- index.php
+++ index.php
@@ -3,12 +3,22 @@
     <title>PHP Test Page</title>
   </head>
   <body>
-    <h1>PHP Test Page, </h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
-    <p> Hello From GitHub!</p>
-    <p>Hello from replit!</p>
-    <p>Hello fom VScode!</p>
+        <h1>Form Handler </h1>
+  <?php
+   if (isset($_POST["FirstName"])){//show data
+      echo $_POST["FirstName"];
+    }else {//show form
+     echo '
+          <form method="post" action="">
+             <p> First Name:<input type="text" name="FirstName"/></p>
+             <p><input type="submit"/></p>
+          </form>
+
+         ';
+   }
+
+      ?>
   </body>
+
+   
 </html>
